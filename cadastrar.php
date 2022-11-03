1e8c03589ba9e4375859c628eba064e0d350911c
Leonardo Nascimento <omgleo@live.com>
2022-11-03 14:17:31 -0300

    crud basico php

---
+++ cadastrar.php
@@ -0,0 +1,14 @@
+<h1>Cadastrar usuario</h1>
+
+<form  method = "POST" action="cadastrar_action.php">
+
+<label >
+Nome: <input type="text" name = "nome">
+</label>
+<label >
+Email: <input type="text" name ="email">
+</label>
+<input type="submit" value="Salvar">
+
+
+</form>
